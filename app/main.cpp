4fbf74611a91806da612ca2bdfaee7dcfa1206d8
kamfor <kfforex@gmail.com>
2016-04-15 08:48:51 +0200

    maincorrect

--- app/main.cpp
+++ app/main.cpp
@@ -21,28 +21,33 @@ int main()
 
     cout<<"Type parity type name from list below\n"<<endl;
     connection.parity.showoptions();
-    cin>>i;
-    connection.parity.set(i);
+    do {
+        cin>>i;
+    }while(connection.parity.set(i));
 
     cout<<"Type stop bits name from list below\n"<<endl;
     connection.stopbits.showoptions();
-    cin>>i;
-    connection.stopbits.set(i);
+    do{
+        cin>>i;
+    }while(connection.stopbits.set(i));
 
     cout<<"Type data bits name from list below\n"<<endl;
     connection.databits.showoptions();
-    cin>>i;
-    connection.databits.set(i);
+    do{
+        cin>>i;
+    }while(connection.databits.set(i));
 
     cout<<"Type port name from list below\n"<<endl;
     connection.portname.showoptions();
-    cin>>i;
-    connection.portname.set(i);
+    do{
+        cin>>i;
+    }while(connection.portname.set(i));
 
     cout<<"Type flow control type from list below\n"<<endl;
     connection.flowcontrol.showoptions();
-    cin>>i;
-    connection.flowcontrol.set(i);
+    do{
+        cin>>i;
+    }while(connection.flowcontrol.set(i));
 
 
 
