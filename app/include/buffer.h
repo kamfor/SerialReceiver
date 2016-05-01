d42548cafd5b932fd6296bb5731fe30cb191e2e6
kamfor <kfforex@gmail.com>
2016-05-01 20:55:27 +0200

    dox

--- app/include/buffer.h
+++ app/include/buffer.h
@@ -1,6 +1,14 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
+/*!
+* \file
+* \brief Definicja klasy Buffer
+*
+* Plik zawiera definicję klasy Buffer, która
+* odpowiada za obsługę bufora odbieranych danych
+*/
+
 #include <iostream>
 using namespace std;
 
