8dcf7b7ae94bfc49ff26459e2761d31891705a82
kamfor <kfforex@gmail.com>
2016-05-24 12:29:55 +0200

    doxy

--- SerialGUI/include/plot.h
+++ SerialGUI/include/plot.h
@@ -8,13 +8,34 @@ namespace Ui {
 class Plot;
 }
 
+//!  QDialog class for handling plot finctions
+/*!
+  Class contains methods to handle and generate realdata plots.
+*/
 class Plot : public QDialog
 {
     Q_OBJECT
 
 public:
+    //! A constructor.
+    /*!
+      Constructor
+    */
     explicit Plot(QWidget *parent = 0);
+
+    //! A destructor.
+    /*!
+      A more elaborate description of the destructor.
+    */
     ~Plot();
+
+    //! A normal member taking two arguments and returning an integer value.
+    /*!
+      \param a an integer argument.
+      \param s a constant character pointer.
+      \return The test results
+      \sa QTstyle_Test(), ~QTstyle_Test(), testMeToo() and publicVar()
+    */
     void generatePlot();
 
 
@@ -23,6 +44,12 @@ private:
     QCustomPlot *customPlot;
 
 private slots:
+    //! A pure virtual member.
+    /*!
+      \sa testMe()
+      \param c1 the first argument.
+      \param c2 the second argument.
+    */
     void realtimeDataSlot(double,double);
 
 };
